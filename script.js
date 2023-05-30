c61f7659309dd339a0daf264f2d2d011c0feff9a
Gertrum <134978515+Gertrum@users.noreply.github.com>
2023-05-30 16:32:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const img = document.createElement('img');
-
-window.addEventListener('load', function() {
-  var modal = document.createElement('div');
-  modal.className = 'modal';
-
-  var modalContent = document.createElement('div');
-  modalContent.className = 'modal-content';
-  modalContent.innerHTML = '<p>Хочешь увидеть, что в темноте?</p><button id="jumpButton">заглянуть во тьму</button>';
-  
-  modal.appendChild(modalContent);
-  document.body.appendChild(modal);
-
-  var jumpButton = document.getElementById('jumpButton');
-  jumpButton.addEventListener('click', function() {
-    modal.style.display = 'none';
-    alert('https://vk.com/gertrum_ph');
-  });
-  modal.style.display = 'block';
-});
+function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+            function myFunction() {
+                (async () => {
+                    let response = await fetch('https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1');
+                    let el = document.getElementById('demo')
+                    el.innerHTML = ""
+                    let text = await response.text(); // прочитать тело ответа как текст
+                    for (const [key, value] of Object.entries(JSON.parse(text)[0])) {
+                        const newEl = document.createElement("li")
+                        newEl.appendChild(document.createTextNode(`${key}: ${value}`))
+                        el.appendChild(newEl)
+                        await sleep(100)
+                    }
+                })()
